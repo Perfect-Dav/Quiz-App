f3e08af1fe61b91ad00b20562251dcd2087bfc54
Perfect-Dav <perfectadewale31@gmail.com>
2021-01-02 15:32:42 +0100

    404 page done

--- src/components/404/NoPage.js
+++ src/components/404/NoPage.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import { Helmet } from "react-helmet";
+
+import{ Box, Button } from "@material-ui/core";
+
+//Images
+import topImg from "./assets/404image.png";
+
+//Styles
+import styles from "./nopage.module.css";
 
 const NoPage = () => {
     return ( 
@@ -7,9 +16,34 @@ const NoPage = () => {
           <Helmet>
               <title>404 | Page Not Found</title>
           </Helmet>
-          
-            <section>
-                <h2>NOT FOUND</h2>
+
+            <section className={styles.noPageBg}>
+                <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    paddingY={10}
+                    bgcolor="#2f49b0"
+                    width="100%"
+                    height="100%"
+                    style={{ padding: '20px' }}
+                >
+                    <div>
+                        <img 
+                            src={topImg}
+                            alt="404 logo"
+                            className="img-fluid"
+                        />
+                        <h1>404 | PAGE NOT FOUND !</h1>
+                        <h2>Sure you know the page you are looking for?</h2>
+
+                        <Button component={RouterLink} to="/" className={styles.homeButton}>
+                            Go Back Home
+                        </Button>
+
+                    </div>
+                </Box>
+                
             </section>
         </>
      );
